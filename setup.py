b31850c9c7b61d456f8a2f18bc786ed5f9b1a420
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-12-13 21:00:38 +0000

    Enforce a minimum Trac version in `setup.py`. Refs trac:#11388.
    The method `disable_component` in `trac.core:ComponentManager`, which is utilized by this plugin in `estimationtools.utils`, is only available since Trac 0.11.3 (see trac:#7748, [trac 7719]).  Thanks to Jun Omae for the hint.
    
    git-svn-id: https://trac-hacks.org/svn/estimationtoolsplugin/trunk@13472 7322e99d-02ea-0310-aa39-e9a107903beb

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 
+
+import sys
 from setuptools import setup
+from pkg_resources import parse_version
+
+
+name = 'EstimationTools'
+version = '0.4.6'
+min_trac = '0.11.3'
+try:
+    import trac
+    if parse_version(trac.__version__) < parse_version(min_trac):
+        print "%s %s requires Trac >= %s" % (name, version, min_trac)
+        sys.exit(1)
+except ImportError:
+    pass
 
 setup(
-    name = 'EstimationTools',
+    name = name,
     author = 'Joachim Hoessler',
     author_email = 'hoessler@gmail.com',
     description = 'Trac plugin for visualizing and quick editing of effort estimations',
-    version = '0.4.6',
+    version = version,
     license='BSD',
     packages=['estimationtools'],
     package_data = { 'estimationtools': ['htdocs/*.js', 'templates/*.html'] },
